5ab5beaeaaf89a927693e6ab29d12a81ad3105f0
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-06-14 13:15:45 -0500

    Add bidders and bidder_info methods

--- lib/attendee.rb
+++ lib/attendee.rb
@@ -10,4 +10,35 @@ class Attendee
   def budget
     @budget.delete('$').to_i
   end
+
+  def bidders
+    names = []
+    items.each do |item|
+      items.bids.each do |bid|
+        names << bid[0].name
+      end
+    end
+    names.uniq
+  end
+
+  def bidder_info
+    bidder_info = Hash.new()
+    items.each do |item|
+      item.bids.each do |bid|
+        bidder_info[bid[0]] = {}
+      end
+    end
+    bidder_info.each do |key, value|
+      items.each do |item|
+        value[:budget] = key.budget
+        if item.bids.keys.include?(key)
+          if value[:items] == nil
+            value[:items] = [item.name]
+          else
+            value[:items] += [item.name]
+          end
+        end
+      end
+    end
+  end
 end
