769ee7e109070e28023eb2a7b4913c7863e04d31
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-06-14 11:39:22 -0500

    test: Add and pass add bids test

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,4 +1,6 @@
 require './lib/item'
+require './lib/attendee'
+# require './lib/auction'
 RSpec.describe Item do
   it "exists and has attributes" do
     item1 = Item.new('Chalkware Piggy Bank')
@@ -7,8 +9,18 @@ RSpec.describe Item do
     expect(item1.name).to eq("Chalkware Piggy Bank")
   end
 
-  # it " " do
+  it "can add bids" do
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
 
-  #   expect().to be_a()
-  # end
+    expect(item1.bids).to eq({})
+    attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+
+    item1.add_bid(attendee2, 20)
+    item1.add_bid(attendee1, 22)
+    
+
+    expect(item1.bids).to eq({attendee2 => 20, attendee1 => 22})
+  end
 end
